a88a29d7c618cfc1d049544bae84c6afee10d8a1
Maurice Faber <morriz@idiotz.nl>
2018-10-22 01:09:42 +0200

    bump

--- flux-install.sh
+++ flux-install.sh
@@ -1,6 +1,6 @@
 helm install --name team-frontend-api-flux \
 --set rbac.create=true \
 --set helmOperator.create=true \
---set git.url=ssh://git@github.com/morriz/nodejs-demo-api \
+--set git.url=https://github.com/Morriz/nodejs-demo-api.git \
 --namespace team-frontend \
 weaveworks/flux
