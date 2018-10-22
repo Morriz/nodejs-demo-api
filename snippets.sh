8457183fd8c40bc57b6155fd760677ee86aec222
Maurice Faber <morriz@idiotz.nl>
2018-10-22 04:29:39 +0200

    doh

--- snippets.sh
+++ snippets.sh
@@ -24,3 +24,10 @@ kubectl -n dev create secret generic basic-auth \
 kubeseal --format=yaml --cert=pub-cert.pem < basic-auth.json > releases/dev/basic-auth.yaml
 
 rm basic-auth.json
+
+# To prepare for disaster recovery you should backup the sealed-secrets controller private key with:
+kubectl get secret -n team-frontend sealed-secrets-key -o yaml --export > sealed-secrets-key.yaml
+
+# To restore from backup after a disaster, replace the newly-created secret and restart the controller:
+kubectl replace secret -n team-frontend sealed-secrets-key -f sealed-secrets-key.yaml
+kubectl delete pod -n team-frontend -l app=sealed-secrets
